c5094af072254b96149ee0d6721ea3b6c4a01730
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2017-04-08 23:09:35 +0300

    finalized tests and added page-title class

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -14,7 +14,6 @@ describe('Countdown', () => {
   describe('handleSetCountdown', () => {
     it('should set state to "started" and count down', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
-
       countdown.handleSetCountdown(10);
 
       expect(countdown.state.count).toBe(10);
@@ -28,13 +27,36 @@ describe('Countdown', () => {
 
     it('should never set "count" less than 0', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
-
       countdown.handleSetCountdown(1);
 
       setTimeout(() => {
         expect(countdown.state.count).toBe(0);
         done();
       }, 3000);
+    });
+
+    it('should pause countdown on "paused" status', (done)=> {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should reset count on "stopped" status', (done)=> {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
     });
 
 
