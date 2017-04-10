41bc734e4abfd91e41b367c0b6d94c71f30145e2
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2017-04-10 10:41:06 +0300

    support foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -6,7 +6,6 @@ var Timer = require('Timer');
 var Countdown = require('Countdown');
 
 // load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 //App css
